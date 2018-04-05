efdf1ff98e62c721103a7ebd076818a6852fa3ef
Michael Gonzalez <mikegonz@udel.edu>
2018-04-05 23:46:48 +0200

    added asset folders with stuff in them, just played around in my scene

---
+++ Assets/Scripts/CowScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CowScript : MonoBehaviour {
+
+	private float alphaDelta = 0.005f;
+
+	public Material cowMat;
+	// Use this for initialization
+	void Start () {
+		Color origColor = cowMat.color;
+		cowMat.color = new Color (cowMat.color.r, cowMat.color.g, cowMat.color.b, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (cowMat.color.a < 0)
+			alphaDelta = 0.005f;
+		if (cowMat.color.a > 1f)
+			alphaDelta = -0.005f;
+		
+		cowMat.color = new Color (cowMat.color.r, cowMat.color.g, cowMat.color.b, cowMat.color.a + alphaDelta);
+	}
+}
