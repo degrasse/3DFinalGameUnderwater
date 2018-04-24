3c2f73c924d170a9072ffa0048534d586a9a65f2
Michael Gonzalez <mikegonz@udel.edu>
2018-04-24 11:22:47 +0200

    more terrain stuff

---
+++ Assets/Scripts/StrangerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrangerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.B)) {
+			StartCoroutine (BobHead (3.0f));
+		}
+		if (Input.GetKeyDown (KeyCode.S)) {
+			StartCoroutine (ShakeHead (3.0f));
+		}
+	}
+
+	IEnumerator BobHead(float duration){
+		for (int c = 0; c < duration * 10; c++) {
+			yield return new WaitForSeconds (0.1f);
+		}
+	}
+
+	IEnumerator ShakeHead(float duration){
+		for (int c = 0; c < duration * 10; c++) {
+			yield return new WaitForSeconds (0.1f);
+		}
+	}
+}
