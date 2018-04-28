05116fb556726f1a7965f9b14877b850438fc562
phetzler <phetzler@u.rochester.edu>
2018-04-28 21:02:15 +0200

    Added Comments

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,88 +4,78 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
-	public GameObject playerLight;
-	public GameObject[] oxygenTanks;
-	public float initOxygen, minLightAngle, minLightIntensity;
-	public GameObject terrain; 
+	public GameObject playerLight; //light that the player uses to see
+	public GameObject[] oxygenTanks; //array of collectible oxygen tanks
+	public float initOxygen, minLightAngle, minLightIntensity; //initial oxygen in seconds, minimum light angle, minimum light intensity
+	public GameObject terrain; //terrain in game
 
 	private GameObject pauseMenu;
-	private bool paused;
-	private Light spotLight;
-	private float oxygenLeft;
-	private int currentTank;
-	private TerrainScript terrScript;
-	private Coroutine waveCoroutine;
+	private bool paused; //tell if game is paused or not
+	private Light spotLight; //spot light attached to player
+	private float oxygenLeft; //amount of oxygen left in seconds
+	private int currentTank; //current tank to collect
+	private TerrainScript terrScript; //script on terrain game object
+	private Coroutine waveCoroutine; //coroutine that runs wave through terrain
 
 
-	void Start () {
+	void Start () { //initialize variables
 		waveCoroutine = null;
 		oxygenLeft = initOxygen;
 		spotLight = playerLight.GetComponent<Light> ();
 		spotLight.type = LightType.Spot;
 		currentTank = 0;
-		if (oxygenTanks.Length == 0) {
-			Debug.Log ("No Oxygen Tanks");
-		} else {
-			/*
-			for (int i = 1; i < oxygenTanks.Length; i++) {
-				oxygenTanks [i].SetActive (true);
-			}
-			//*/
-		}
 
-		//StartCoroutine (beginGame());
 	}
 
 
 	IEnumerator beginGame(){
-		//Time.timeScale = 0f;
-
-		yield return new WaitForSecondsRealtime (3f);
-		Time.timeScale = 0f;
-		terrScript.InitialRaise();
+		yield return new WaitForSecondsRealtime (3f); //wait so player has time to look around and see goals
+		Time.timeScale = 0f; //stop player movement
+		terrScript.InitialRaise(); //raise terrain to put the player in the environment
 		yield return new WaitForSecondsRealtime (10f);
-		Time.timeScale = 1f;
+
+		//<------------------------------------------------------------------------------------------------------------CHANGE TO WAIT TIL RAISE IS DONE THEN RESUME
+		//<---------------------------ADD DISABLE OXYGEN TANKS
+
+		Time.timeScale = 1f; //resume time
 	}
 
 
 
-	void Awake() {
+	void Awake() { //initialize variables from other scripts and objects
 		paused = false;
 		pauseMenu = GameObject.Find ("PauseMenu");
 		pauseMenu.SetActive (paused);
 		terrScript = terrain.GetComponent<TerrainScript> ();
-		StartCoroutine (beginGame());
-		Time.timeScale = 1.0f;
+		StartCoroutine (beginGame()); //start coroutine that sets the game area up properly
 	}
 
 	void Update () {
-		if (!paused) {
+		if (!paused) { //if the game is not paused count down oxygen left
 			oxygenLeft -= Time.deltaTime;
-			updatePlayerLight ();
+			updatePlayerLight (); //update the players spot light
 		}
-		if (Input.GetKeyDown (KeyCode.Escape)) {
+		if (Input.GetKeyDown (KeyCode.Escape)) { //if the player hits escape change the pause menu
 			TogglePauseMenu ();
 		}
 	}
 
 	void updatePlayerLight (){
 		
-		///*
-		if (oxygenLeft > 45) { //set initial light
-			if (waveCoroutine != null) {
-				StopCoroutine (waveCoroutine);
+		if (oxygenLeft > 45) { //light setting for  greater than 45 seconds of oxygen left
+			if (waveCoroutine != null) { //if the wave routine is running
+				StopCoroutine (waveCoroutine); //stop it and set to null
 				waveCoroutine = null;
 			}
-			spotLight.spotAngle = 120;
-			spotLight.color = Color.white;
-			spotLight.intensity = 3f;
-		} else if (oxygenLeft > 5) {
-			spotLight.spotAngle = ((120f - minLightAngle) * ((oxygenLeft - 5f) / 40f) + minLightAngle); //slowly shrink light angle
-			if (oxygenLeft <= 15) {
-				spotLight.spotAngle += 5f * Mathf.Sin ((oxygenLeft-15f) * 2); //pulse light angle
-				spotLight.intensity = 3f + 2f * Mathf.Sin ((oxygenLeft-15f)); //wave of intensity
-				if (waveCoroutine == null) {
+			spotLight.spotAngle = 120; //120 degree angle for light
+			spotLight.color = Color.white; //make sure light color is white
+			spotLight.intensity = 3f; //intensity set to three;
+		} else if (oxygenLeft > 5) { //oxygen left ranges from 5 to 45 seconds
+			spotLight.spotAngle = ((120f - minLightAngle) * ((oxygenLeft - 5f) / 40f) + minLightAngle); //slowly shrink light angle to minimum light angle
+			if (oxygenLeft <= 15) { //oxygen left ranges from 5 to 15 seconds
+				spotLight.spotAngle += 5f * Mathf.Sin ((oxygenLeft-15f) * 2); //pulse light angle based on sin wave
+				spotLight.intensity = 3f + 2f * Mathf.Sin ((oxygenLeft-15f)); //pulse intensity
+				if (waveCoroutine == null) { //if 
 					waveCoroutine = StartCoroutine(terrScript.Wave());
 				}
 			}
