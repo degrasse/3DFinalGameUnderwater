084ca6d555b2109eeabd160745f16a57180c0c39
Michael Gonzalez <mikegonz@udel.edu>
2018-04-29 21:25:55 +0200

    fixed cow bouncing and killed COWMAT ONCE AGAIN

--- Assets/Scripts/CowScript.cs
+++ Assets/Scripts/CowScript.cs
@@ -4,23 +4,35 @@ using UnityEngine;
 
 public class CowScript : MonoBehaviour {
 
-	private float alphaDelta = 0.001f;
+	public float bounceFreq;
+	public float bounceHeight;
 
-	public Material cowMat;
+	private float alphaDelta = 0.001f;
+	private float origy;
+
 	// Use this for initialization
 	void Start () {
-		Color origColor = cowMat.color;
-		cowMat.color = new Color (cowMat.color.r, cowMat.color.g, cowMat.color.b, 0);
+		origy = gameObject.transform.position.y;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
-		if (cowMat.color.a < 0)
-			alphaDelta = 0.001f;
-		if (cowMat.color.a > 1f)
-			alphaDelta = -0.1f;
-		
-		cowMat.color = new Color (cowMat.color.r, cowMat.color.g, cowMat.color.b, cowMat.color.a + alphaDelta);
+		if (Input.GetKeyDown (KeyCode.B)) {
+			StartCoroutine (Bounce ());
+		}
+	}
+
+	IEnumerator Bounce(){
+		float offsetx = -0.493f;
+		float modval = 1.4809f;
+		float freqmult = 20f;
+		float startTime = Time.time;
+		while (true) {
+			float dtime = modval - ((bounceFreq * (Time.time - startTime / 100f)) % modval);
+			gameObject.transform.position = new Vector3(gameObject.transform.position.x, 
+				origy + bounceHeight * Mathf.Abs(Mathf.Sin(Mathf.Sqrt(freqmult * (dtime - offsetx)))),
+				gameObject.transform.position.z);
+			yield return new WaitForSeconds (0.01f);
+		}
 	}
 }
---
+++ Assets/Scripts/EncouragementScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EncouragementScript : MonoBehaviour {
+
+	public Transform playerTransform;
+
+	private TextMesh tm;
+
+	string encouragement1 = "youre doing great kiddo\nyour mother and i are so\nproud";
+
+	// Use this for initialization
+	void Start () {
+		tm = GetComponentInChildren<TextMesh> ();
+		tm.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Mathf.Abs (((Vector3)(playerTransform.position - gameObject.transform.position)).magnitude) < 20f) {
+			tm.text = encouragement1;
+		} else {
+			tm.text = "";
+		}
+	}
+}
