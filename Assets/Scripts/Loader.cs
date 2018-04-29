17d58953010935e788573f200827dd94b48162c2
phetzler <phetzler@u.rochester.edu>
2018-04-29 20:05:10 +0200

    Revert "Revert "Readded game end scenes and commented my code""
    This reverts commit e6d885e7c60ffb33d72b12c419d8c11ee9c83f6a.

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -7,13 +7,26 @@ public class Loader : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	public void Load(){
-	
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 	}
+
+	public void LoadEasy(){ //set easy oxygen amount and load the game
+		GameObject.Find ("OxygenTracker").GetComponent<OxygenTrackerScript> ().setInitOxygen (105, true);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+	public void LoadMedium(){ //set medium oxygen amount and load the game
+		GameObject.Find ("OxygenTracker").GetComponent<OxygenTrackerScript> ().setInitOxygen (90, true);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+	public void LoadHard(){ //set hard oxygen amount and load the game
+		GameObject.Find ("OxygenTracker").GetComponent<OxygenTrackerScript> ().setInitOxygen (75, true);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
 
 	public void Quit()
 	{
