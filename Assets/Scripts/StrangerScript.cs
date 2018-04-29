7cda2466155a228ca629b2196585bf589bddcda9
Michael Gonzalez <mikegonz@udel.edu>
2018-04-29 12:26:58 +0200

    added interpolation curve to stranger movement

--- Assets/Scripts/StrangerScript.cs
+++ Assets/Scripts/StrangerScript.cs
@@ -22,9 +22,12 @@ public class StrangerScript : MonoBehaviour {
 	}
 
 	IEnumerator Move(){
+		int atanGrade = 6;
+		float atanHeight = 2.55f;
 		for (int i = 0; i < moveDuration * 20; i++) {
 			float i2 = i * 1.0f / (moveDuration * 20);
-			gameObject.transform.position = startPos + (endPos - startPos) * i2;
+			float t = (Mathf.Atan(atanGrade*i2 - atanGrade * 0.5f) / atanHeight) + 0.5f;
+			gameObject.transform.position = startPos + (endPos - startPos) * t;
 			yield return new WaitForSeconds (0.05f);
 		}
 	}
